989b59765c98e9a6cb845f78c609bc7b44638ae6
Carlornberg <carlornberg36@gmail.com>
2022-03-28 18:42:09 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -21,8 +21,7 @@ def fixInput(inData):
                 M.append(inData_split[p:p+N+1])
     W = sorted(W) #Python can sort list of lists by first element with this!
     M = sorted(M)
-    end = time.time()
-    print("TIME TO FIX INPUT: " + str(end-start)) 
+    print("TIME TO FIX INPUT: " + str(  time.time() -start)) 
     return N, W, M
 
 def findManList(MenTemp,hisIndex,N): #UNUSED!
@@ -57,7 +56,6 @@ def GS(N,Men, Women):
         her = hisPreferences[nextManProposal[him-1]]
         herPreferences = Women[her-1][1:]
         currentFiancee = womensPartner[her-1] 
-
         if currentFiancee == None:
             womensPartner[her-1] = him
             mensPartner[him-1] = her
@@ -76,14 +74,11 @@ def GS(N,Men, Women):
                 womensPartner[her-1] = him
                 mensPartner[him-1] = her
                 nextManProposal[him-1] = nextManProposal[him-1] + 1
-
                 bachelors.pop()
-                bachelors.insert(0,currentFiancee)
+                bachelors.insert(-1,currentFiancee)
             else:
                 nextManProposal[him-1] = nextManProposal[him-1] + 1
-
-    end = time.time()
-    print("GALE-SHAPNEY METHOD'S TIME: " + str(end-start))
+    print("GALE-SHAPNEY METHOD'S TIME: " + str( time.time()- start))
     return mensPartner
     
 def test(string):
@@ -97,8 +92,7 @@ if __name__ == '__main__':
     start = time.time()
     input = sys.stdin.read()
     # input = open("C:/Users/Carl/Programmering/EDAF05-labs-public/1stablemarriage/data/secret/2testmid.in").read()
-    end = time.time()
-    print("TIME TO READ FILE FROM stdin-METHOD: " + str(end-start))
+    print("TIME TO READ FILE FROM stdin-METHOD: " + str(time.time() -start))
     # input = "4\n4 2 1 4 3\n1 3 2 4 1\n1 1 4\n3 2 2 2\n4 3 1 3 1 2 4\n3 3 4 3 1\n2 4\n3 2 4 1 2 1 3 2\n4"
     test(input)
    
