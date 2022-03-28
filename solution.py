dfc1d504cf899dcf68af8c7dbb9d5cf4c1b2eb30
Carlornberg <carlornberg36@gmail.com>
2022-03-28 19:52:55 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,7 +1,9 @@
-import sys, time
+import sys
+import time
+
 
 def fixInput(inData):
-    start = time.time()
+    # start = time.time()
     inData_split = inData.split()
     N = int(inData_split.pop(0))
     inData_split = [int(i) for i in inData_split]
@@ -9,90 +11,71 @@ def fixInput(inData):
     lenWind = 0
     W = []
     M = []
-    for p in range(0,len(inData_split)-1, N+1):
-        if lenWind == N: #Unnecessary to search in Wind if it is already filled.
+    for p in range(0, len(inData_split)-1, N+1):
+        if lenWind == N:  # Unnecessary to search in Wind if it is already filled.
             M.append(inData_split[p:p+N+1])
         else:
             if inData_split[p] not in Wind:
                 Wind.append(inData_split[p])
-                lenWind= lenWind+1
+                lenWind = lenWind+1
                 W.append(inData_split[p:p+N+1])
             else:
                 M.append(inData_split[p:p+N+1])
-    W = sorted(W) #Python can sort list of lists by first element with this!
+    W = sorted(W)  # Python can sort list of lists by first element with this!
     M = sorted(M)
-    print("TIME TO FIX INPUT: " + str(  time.time() -start)) 
+    # print("TIME TO FIX INPUT: " + str(time.time() - start))
     return N, W, M
 
-def findManList(MenTemp,hisIndex,N): #UNUSED!
-    for man in MenTemp:
-        if man[0] == hisIndex:
-            man = man[1:N+1]
-            return man
-    return None
 
-def findWomanList(WomenTemp, herIndex,N): #UNUSED!
-    for woman in WomenTemp:
-        if woman[0] == herIndex:
-            woman = woman[1:N+1]
-            return woman
-    return None
-
-def GS(N,Men, Women):
-    start = time.time()
+def GS(N, Men, Women):
+    # start = time.time()
     bachelors = []
     for i in Men:
         bachelors.append(i[0])
-    
     mensPartner = [None]*N
-
     womensPartner = [None]*N
-
     nextManProposal = [0]*N
-
     while bachelors:
         him = bachelors[-1]
-        hisPreferences = Men[him-1][1:]
-        her = hisPreferences[nextManProposal[him-1]]
-        herPreferences = Women[her-1][1:]
-        currentFiancee = womensPartner[her-1] 
+        hisPref = Men[him-1][1:]
+        her = hisPref[nextManProposal[him-1]]
+        herPref = Women[her-1][1:]
+        currentFiancee = womensPartner[her-1]
         if currentFiancee == None:
             womensPartner[her-1] = him
             mensPartner[him-1] = her
-
             nextManProposal[him-1] = nextManProposal[him-1] + 1
-
             bachelors.pop()
         else:
 
-            idx = herPreferences.index(currentFiancee)
-
-
-
-            hisIdx = herPreferences.index(him)
+            idx = herPref.index(currentFiancee)
+            hisIdx = herPref.index(him)
             if hisIdx < idx:
                 womensPartner[her-1] = him
                 mensPartner[him-1] = her
                 nextManProposal[him-1] = nextManProposal[him-1] + 1
                 bachelors.pop()
-                bachelors.insert(-1,currentFiancee)
+                bachelors.insert(-1, currentFiancee)
             else:
                 nextManProposal[him-1] = nextManProposal[him-1] + 1
-    print("GALE-SHAPNEY METHOD'S TIME: " + str( time.time()- start))
+    # print("GALE-SHAPNEY METHOD'S TIME: " + str(time.time() -  start))
     return mensPartner
-    
+
+
 def test(string):
     #string = "2\n1 1 2\n2 2 1\n1 1 2\n2 2 1"
     N, W, M = fixInput(string)
-    MenPairs = GS(N,W,M)
+    MenPairs = GS(N, W, M)
     # for i in MenPairs:
     #     print(i)
+    for i in range(0,len(MenPairs)-1):
+        print(MenPairs[i])
+    print(MenPairs[len(MenPairs)-1], end = "")
 
 if __name__ == '__main__':
-    start = time.time()
+    # start = time.time()
     input = sys.stdin.read()
-    # input = open("C:/Users/Carl/Programmering/EDAF05-labs-public/1stablemarriage/data/secret/2testmid.in").read()
-    print("TIME TO READ FILE FROM stdin-METHOD: " + str(time.time() -start))
+    # input = open("C:/Users/Carl/Programmering/EDAF05-labs-public/1stablemarriage/data/sample/2.in").read()
+    # print("TIME TO READ FILE FROM stdin-METHOD: " + str(time.time() - # start))
     # input = "4\n4 2 1 4 3\n1 3 2 4 1\n1 1 4\n3 2 2 2\n4 3 1 3 1 2 4\n3 3 4 3 1\n2 4\n3 2 4 1 2 1 3 2\n4"
     test(input)
-   
