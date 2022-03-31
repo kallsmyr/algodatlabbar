1c8531eb8a5794bc3e021fbb648ee578ede86b71
Carlornberg <carlornberg36@gmail.com>
2022-03-31 12:15:54 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,81 +1,73 @@
 import sys
 import time
 
-
-def fixInput(inData):
-    # start = time.time()
-    inData_split = inData.split()
-    N = int(inData_split.pop(0))
-    inData_split = [int(i) for i in inData_split]
+def input():
+    N = int(sys.stdin.readline()) #Read first line which is always the single integer.
+    remainder = 2*N*(N+1)
+    nbrs = [0]*remainder
+    while remainder > 0: #get the numbers in integer form from input
+        idx = 2*N*(N+1) - remainder
+        line = [int(i) for i in sys.stdin.readline().split()]
+        nbrs[idx:idx+len(line)] = line
+        remainder -= len(line)
+    M = []
+    W = []
     Wind = []
     lenWind = 0
-    W = []
-    M = []
-    for p in range(0, len(inData_split)-1, N+1):
-        if lenWind == N:  # Unnecessary to search in Wind if it is already filled.
-            M.append(inData_split[p:p+N+1])
+    for p in range(0,len(nbrs), N+1):
+        if lenWind ==N:
+            M.append(nbrs[p:p+N+1])
         else:
-            if inData_split[p] not in Wind:
-                Wind.append(inData_split[p])
-                lenWind = lenWind+1
-                W.append(inData_split[p:p+N+1])
+            if nbrs[p] not in Wind:
+                Wind.append(nbrs[p])
+                lenWind +=1
+                W.append(nbrs[p:p+N+1])
             else:
-                M.append(inData_split[p:p+N+1])
-    W = sorted(W)  # Python can sort list of lists by first element with this!
-    M = sorted(M)
-    # print("TIME TO FIX INPUT: " + str(time.time() - start))
-    return N, W, M
+                M.append(nbrs[p:p+N+1])
+    W = sorted(W, reverse = False)  # Python can sort list of lists by first element with this!
+    M = sorted(M, reverse = False)
+    return M, W
 
 
-def GS(N, Men, Women):
-    # start = time.time()
-    bachelors = []
-    for i in Men:
-        bachelors.append(i[0])
-    mensPartner = [None]*N
-    womensPartner = [None]*N
-    nextManProposal = [0]*N
+def MenByIndex(Women): #The GS algorithm produces the stable matching which is worst for women >:(
+#We ought to store each woman´s preferences as indexes.
+    N = len(Women)
+    newW = [None]*N
+    temp = [0]*(N*(N+1))
+    for i_w in range(0,N):
+        temp[i_w*(N+1)] = i_w +1 #women index w_i = i_w +1
+        #fixes python indexing 0->N-1
+        for m in range(1,N+1):
+            pman = Women[i_w][m] #The preferred m:th man for woman i
+            temp[i_w*(N+1) + pman] = m
+        newW[i_w] = temp[i_w * (N+1) : (i_w + 1)*(N+1)]
+    return newW
+
+def GS(M,W):
+    bachelors = []  #Bachelors, format is 
+    marriedWomen = [None]*len(W) #married women
+    W = MenByIndex(W)
+    for m in M:
+        bachelors.append([m[0],1])
+    
     while bachelors:
-        him = bachelors[-1]
-        hisPref = Men[him-1][1:]
-        her = hisPref[nextManProposal[him-1]]
-        herPref = Women[her-1][1:]
-        currentFiancee = womensPartner[her-1]
-        if currentFiancee == None:
-            womensPartner[her-1] = him
-            mensPartner[him-1] = her
-            nextManProposal[him-1] = nextManProposal[him-1] + 1
-            bachelors.pop()
+        him = bachelors.pop()
+        her = M[him[0]-1][him[1]]
+        him[1] = him[1] + 1
+        #  I förra koden sparade vi här i princip currentFiancee = marriedWomen[her-1]
+        if marriedWomen[her-1] is None:
+            marriedWomen[her-1] = him
+        elif W[her-1][him[0]] < W[her-1][marriedWomen[her-1][0]]:
+            bachelors.append(marriedWomen[her-1])
+            marriedWomen[her-1] = him
         else:
+            bachelors.append(him)
+    for i in range(0,len(marriedWomen)):
+        print(marriedWomen[i][0])
 
-            idx = herPref.index(currentFiancee)
-            hisIdx = herPref.index(him)
-            if hisIdx < idx:
-                womensPartner[her-1] = him
-                mensPartner[him-1] = her
-                nextManProposal[him-1] = nextManProposal[him-1] + 1
-                bachelors.pop()
-                bachelors.insert(-1, currentFiancee)
-            else:
-                nextManProposal[him-1] = nextManProposal[him-1] + 1
-    # print("GALE-SHAPNEY METHOD'S TIME: " + str(time.time() -  start))
-    return mensPartner
-
-
-def test(string):
-    #string = "2\n1 1 2\n2 2 1\n1 1 2\n2 2 1"
-    N, W, M = fixInput(string)
-    MenPairs = GS(N, W, M)
-    # for i in MenPairs:
-    #     print(i)
-    for i in range(0,len(MenPairs)-1):
-        print(MenPairs[i])
-    print(MenPairs[len(MenPairs)-1], end = "")
+def test():
+    M, W = input()
+    GS(M,W)
 
 if __name__ == '__main__':
-    # start = time.time()
-    input = sys.stdin.read()
-    # input = open("C:/Users/Carl/Programmering/EDAF05-labs-public/1stablemarriage/data/sample/2.in").read()
-    # print("TIME TO READ FILE FROM stdin-METHOD: " + str(time.time() - # start))
-    # input = "4\n4 2 1 4 3\n1 3 2 4 1\n1 1 4\n3 2 2 2\n4 3 1 3 1 2 4\n3 3 4 3 1\n2 4\n3 2 4 1 2 1 3 2\n4"
-    test(input)
+    test()
