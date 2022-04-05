e47d0f9328e96d6f10f4c53d8d872d2f7adbab55
kallsmyr <69216613+kallsmyr@users.noreply.github.com>
2022-04-05 14:04:12 +0200

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 10:30:02 2022
+
+@author: Isak
+"""
+
+def arccheck(word1, word2):
+    lettersfound = 0
+    #letters = [start[1], start[2], start[3], start[4]]
+    wordlist = []
+    for k in range(0,5):
+        wordlist.append(word2[k])
+    for i in range (1,5):
+        letter = word1[i]
+        found = False
+        for j in range(0,5):
+            if ((wordlist[j] == letter) and (found == False)):
+                found = True
+                wordlist[j] = '_'
+        if found :
+            lettersfound += 1
+    if lettersfound == 4:
+        return True
+    return False
+
+if __name__ == '__main__':
+    print(arccheck('there', 'where'))
+    
