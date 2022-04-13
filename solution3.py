da219856f1245c7453b15840078535ad08bf7c45
Carlornberg <carlornberg36@gmail.com>
2022-04-13 10:51:05 +0200

    Add files via upload

--- solution3.py
+++ solution3.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 12 10:24:01 2022
-
-@author: kalls
-"""
+import sys
+from collections import deque
 import time
- #\
+
 def formatter():
-    input = open("C:\\Users\\kalls\\OneDrive\\Dokument\\EDAF05-labs-public-master\\3makingfriends\\data\\secret\\4huge.in")
+    input = open("C:\\Users\\Carl\\Programmering\\EDAF05-labs-public\\3makingfriends\\data\\secret\\4huge.in")
     firstline = input.readline()
     space = firstline.find(" ")
     N = int(firstline[:space])
@@ -35,23 +31,24 @@ def formatter():
      #   dict1[i].append(dict2[i])
     #dict1.update(dict2)
     return dict1, dict2
-        
-def people(N,M,pairs,weights): #Behöver därför inte denna
-    dict = {} 
+
+def people(N,pairs,weights): #Dictionary av dictionary (coolt)
+    dict = {} #Key är en person, value är en dictionary för alla dess vänner
+    # Inre dictionary för varje vän: key är vän value är viken av vänskapen 
     for i in range(1,N+1):
         dict.update({i:[]}) 
         for idx,p in enumerate(pairs):
             if (p[0] == i):
-                #dict[i].update({p[1]:weights[idx]})
                 dict[i].append((p[1],weights[idx]))
+                #dict[i].append(p[1])
+                #dict[i[1]].append(weights[idx])
             elif (p[1] == i):
                 dict[i].append((p[0],weights[idx]))
-                #dict[i].update({p[0]:weights[idx]})
-    #for kvp in dict:
-     #   s = sorted(dict[kvp],key=lambda ele:ele[1])
-      #  dict[kvp] = s
-    return dict                
-    
+                #dict[i[0]].append(p[0])
+                #dict[i[1]].append(weights[idx])  
+    for kvp in dict: #Tänk om
+        dict[kvp] = sorted(dict[kvp], key= lambda ele: ele[1])
+    return dict               
 
 def dictmerge(dict1,dict2): #Mergar 2 dictionries så vi inte får listor i listor på values
     for i in dict1:
@@ -68,18 +65,36 @@ def dictmerge(dict1,dict2): #Mergar 2 dictionries så vi inte får listor i list
       #  dict1[i] = s 
     return dict1
 
-def Prim(dict):
-    
-    
-    
-    
-    
-    
-    return 0
-     
+
+def Prim(G,r):
+    T = []
+    totweight = 0
+    Q = deque([])
+    added = [1]
+    for v in r:
+        Q.append(v)
+    while Q:
+        while Q[0][0] in added:
+            Q.popleft()
+            if len(Q) ==0:
+                return totweight
+        added.append(Q[0][0])
+        T.append(Q[0])
+        totweight += Q[0][1]
+        if len(T) == len(G):
+            return totweight
+        
+
+        for tuple in G[Q[0][0]]:
+            if tuple[0] not in added:
+                Q.append(tuple)
+        Q = deque(sorted(Q, key =  lambda ele: ele[1]))
+
+    return totweight       
+
+                
 if __name__ == '__main__':
     start = time.time()
     d1, d2 = formatter()
     d = dictmerge(d1,d2)
-    #print(dgood)
     print("Time to format:" + str(time.time()-start))
